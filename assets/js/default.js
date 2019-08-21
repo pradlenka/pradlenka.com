1887f6f2d24eedf0140d9ecc789c03d58dab8991
Martin Kopta <martin.kopta+github@garcon.cz>
2019-08-21 02:24:34 +0200

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -3,12 +3,12 @@ window.addEventListener('load', function() {
 	const hero_length = hero_images.length;
 	let current_image = 0;
 	const hero = document.querySelector(".hero");
-	console.log((new Image()).src = hero_images[current_image]);
+	(new Image()).src = hero_images[current_image];
 	setTimeout(next, 10000, hero_images, hero_length, current_image, hero);
 })
 function next(hero_images, hero_length, current_image, hero) {
 	hero.style.backgroundImage = "url(" + hero_images[current_image] + ")";
 	current_image = (current_image + 1) % hero_length;
-	console.log((new Image()).src = hero_images[current_image]);
+	(new Image()).src = hero_images[current_image];
 	setTimeout(next, 10000, hero_images, hero_length, current_image, hero);
 }
